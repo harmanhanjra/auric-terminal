4ed52fb837dc10e40282c6d18edc1f88d0566a42
Harmanpreet Singh <harmanpreet.preet06@gmail.com>
2026-09-19 15:45:47 +0530

    test(v3): pin local-safe production controls in test environment

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,8 @@ os.environ["TWELVE_DATA_API_KEY"] = ""
 os.environ["ENABLE_LIVE_TRADING"] = "false"
 os.environ["ENABLE_AUTO_LIVE_TRADING"] = "false"
 os.environ["ENGINE_ENABLED"] = "false"
+os.environ["AURIC_AUTH_REQUIRED"] = "false"
+os.environ["AURIC_EXECUTION_STAGE"] = "paper"
 os.environ["MAX_LOT"] = "1.0"
 os.environ["MAX_DAILY_LOSS"] = "500.0"
 
