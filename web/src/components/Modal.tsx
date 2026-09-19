1b76b0cb93fdfd10f80c1d14494498fb33806f54
Harmanpreet Singh <harmanpreet.preet06@gmail.com>
2026-09-19 21:15:25 +0530

    fix: use canonical Button component casing

--- web/src/components/Modal.tsx
+++ web/src/components/Modal.tsx
@@ -1,6 +1,6 @@
 import { X } from 'lucide-react'
 import { useEffect, type ReactNode } from 'react'
-import { Button } from './ui/button'
+import { Button } from './ui/Button'
 
 interface ModalProps {
   id: string
